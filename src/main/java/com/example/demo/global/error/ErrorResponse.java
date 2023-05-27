2d67361bd9fa950a5f1f777af701e0b7d4a16047
kwx4957 <kdy4957@naver.com>
2023-05-27 23:22:35 +0900

    Feat: ErrorResponse 파일 추가
    - ErrorResponse

---
+++ src/main/java/com/example/demo/global/error/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.example.demo.global.error;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class ErrorResponse {
+    private final String msg;
+}
