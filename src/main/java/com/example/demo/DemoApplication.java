6e40d6c5b22b9d408240be8ea0138469cb5be27e
dansam <33277725+kwx4957@users.noreply.github.com>
2023-06-08 02:08:36 +0900

    Feat: 소셜 로그인 기능 구현 1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -25,16 +25,4 @@ public class DemoApplication {
 		};
 	}
 }
-	//	@Bean
-//	public ApplicationRunner run(TopicRepository topicRepository) throws Exception{
-//		return (ApplicationArguments args) ->{
-//			List<Topic> topics = Arrays.asList(
-//					Topic.builder().title("엉덩이는 2개인가 1개인가").content("내용").firstOption("엉1").secondOption("엉2").password("password").build(),
-//					Topic.builder().title("여름인가 겨울인가").content("내용").firstOption("여름").secondOption("겨울").password("password").build(),
-//					Topic.builder().title("밤 vs 낮").content("내용").firstOption("밤").secondOption("낮").password("password").build(),
-//					Topic.builder().title("치킨 vs 피자").content("내용").firstOption("치킨").secondOption("피자").password("password").build()
-//			);
-//			topicRepository.saveAll(topics);
-//		};
-//	}
 
